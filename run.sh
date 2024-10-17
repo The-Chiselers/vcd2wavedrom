fed3dfe948629e1d69ef2401ec63a3705f112741
Nick Allison <nickrallison1@gmail.com>
2024-10-17 10:39:19 -0600

    updated run and added nix

--- run.sh
+++ run.sh
@@ -2,5 +2,7 @@
 
 mkdir -p out
 
+rm -f out/vcd.json out/wave.json
 ./venv/bin/python3 scripts/main.py --vcd_file ./examples/gpio_all.vcd > out/vcd.json
 cargo run -- out/vcd.json examples/gpio_all.json | tee out/wave.json
+wavedrom-cli --input out/wave.json --svg out/wave.svg
